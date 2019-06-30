b9e78e238a9879dd639f0a3fc8294ee40d28d333
Kaamil Jasani <kaamilj@yahoo.com>
2019-06-30 20:07:20 +0100

    Crafting ingredients display fix

--- src/main/java/com/extracraftx/minecraft/serveradditionsutil/mixin/ShapelessRecipeMixin.java
+++ src/main/java/com/extracraftx/minecraft/serveradditionsutil/mixin/ShapelessRecipeMixin.java
@@ -1,16 +1,41 @@
 package com.extracraftx.minecraft.serveradditionsutil.mixin;
 
-import com.extracraftx.minecraft.serveradditionsutil.interfaces.ClonableItemStackConsumer;
+import com.extracraftx.minecraft.serveradditionsutil.interfaces.ClientItemStackProvider;
+import com.extracraftx.minecraft.serveradditionsutil.interfaces.ClientRecipeProvider;
 
 import org.spongepowered.asm.mixin.Mixin;
 
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.recipe.Ingredient;
 import net.minecraft.recipe.ShapelessRecipe;
+import net.minecraft.util.DefaultedList;
 
 @Mixin(ShapelessRecipe.class)
-public abstract class ShapelessRecipeMixin implements ClonableItemStackConsumer<ShapelessRecipe>{
+public abstract class ShapelessRecipeMixin implements ClientRecipeProvider<ShapelessRecipe>{
     @Override
-    public ShapelessRecipe clone(ShapelessRecipe original, ItemStack itemStack) {
-        return new ShapelessRecipe(original.getId(), original.getGroup(), itemStack, original.getPreviewInputs());
+    public ShapelessRecipe getClientRecipe(ShapelessRecipe original) {
+        ItemStack output = original.getOutput();
+        Item outputItem = output.getItem();
+        if(outputItem instanceof ClientItemStackProvider){
+            output = ((ClientItemStackProvider)outputItem).getClientItemStack(output);
+        }
+        DefaultedList<Ingredient> ingredients = original.getPreviewInputs();
+        DefaultedList<Ingredient> transformedIngredients = DefaultedList.create();
+        for(Ingredient ingredient : ingredients){
+            ItemStack[] originalStacks = ingredient.getStackArray();
+            ItemStack[] transformedStacks = new ItemStack[originalStacks.length];
+            for(int i = 0; i < originalStacks.length; i++){
+                ItemStack stack = originalStacks[i];
+                Item item = stack.getItem();
+                if(item instanceof ClientItemStackProvider){
+                    transformedStacks[i] = ((ClientItemStackProvider)item).getClientItemStack(stack);
+                }else{
+                    transformedStacks[i] = stack;
+                }
+            }
+            transformedIngredients.add(Ingredient.ofStacks(transformedStacks));
+        }
+        return new ShapelessRecipe(original.getId(), original.getGroup(), output, transformedIngredients);
     }
 }
