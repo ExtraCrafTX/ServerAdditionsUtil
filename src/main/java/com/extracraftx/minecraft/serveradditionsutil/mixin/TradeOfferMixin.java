1214153349b7424b907374f28886ea9e4915ee9e
Kaamil Jasani <kaamilj@yahoo.com>
2019-07-01 20:39:55 +0100

    Fixed trading

---
+++ src/main/java/com/extracraftx/minecraft/serveradditionsutil/mixin/TradeOfferMixin.java
@@ -0,0 +1,36 @@
+package com.extracraftx.minecraft.serveradditionsutil.mixin;
+
+import com.extracraftx.minecraft.serveradditionsutil.interfaces.ClientItemStackProvider;
+import com.extracraftx.minecraft.serveradditionsutil.interfaces.ClientTradeOfferProvider;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.village.TradeOffer;
+
+@Mixin(TradeOffer.class)
+public abstract class TradeOfferMixin implements ClientTradeOfferProvider{
+    
+    @Override
+    public TradeOffer getClientTradeOffer(TradeOffer original) {
+        CompoundTag tag = original.toTag();
+        ItemStack buy = convertStack(ItemStack.fromTag(tag.getCompound("buy")));
+        buy.toTag(tag.getCompound("buy"));
+        ItemStack buyB = convertStack(ItemStack.fromTag(tag.getCompound("buyB")));
+        buyB.toTag(tag.getCompound("buyB"));
+        ItemStack sell = convertStack(ItemStack.fromTag(tag.getCompound("sell")));
+        sell.toTag(tag.getCompound("sell"));
+        return new TradeOffer(tag);
+    }
+
+    private ItemStack convertStack(ItemStack original){
+        Item item = original.getItem();
+        if(item instanceof ClientItemStackProvider){
+            return ((ClientItemStackProvider)item).getClientItemStack(original);
+        }
+        return original;
+    }
+    
+}
